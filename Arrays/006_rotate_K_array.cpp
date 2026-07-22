55b931c4a86eedf76aa4d9397d2ef1794f867b36
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-07-22 21:11:26 +0530

    solved rotate by k

---
+++ Arrays/006_rotate_K_array.cpp
@@ -0,0 +1,15 @@
+class solution{
+    public:
+
+    vector<int>rotatebyk(vector<int>&nums, int k){
+
+        int n = nums.size();
+        k = k%n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);  
+        reverse(nums.begin()+k, nums.end());
+        return nums;
+        
+    }
+}
