7704a3d467ddaadef71882bbb319a18ea2967118
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-07-24 10:46:10 +0530

    solved maximum count ones

---
+++ Arrays/009_maximum_count_one.cpp
@@ -0,0 +1,20 @@
+class solution{
+    public:
+
+    int maxcountone(vector<int>&nums){
+        int count = 0;
+        int ans = 0;
+
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i] ==1){
+                count++;
+                ans = max(ans,count);
+            }
+            else{
+                count = 0;
+            }
+        }
+        return ans;
+    }
+
+};
