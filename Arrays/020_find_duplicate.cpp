3f22d039934d4caa03a7989b0e663e2779a7cd5d
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-07-30 17:44:53 +0530

    solved find duplicate (leet code 287)

---
+++ Arrays/020_find_duplicate.cpp
@@ -0,0 +1,19 @@
+class solution{
+    public:
+
+    int findDuplicate(vector<int> & nums){
+        int ans = -1;
+
+        for(int i =0; i<nums.size(); i++){
+            int index = abs(nums[i]);
+
+            if(nums[index]<0){
+                ans = index;
+                break;
+            }
+
+            nums[index] *= -1;
+        }
+        return ans;
+    }
+}
