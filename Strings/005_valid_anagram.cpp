4e8e80f1033d3f52c73cb28094c9140217866bba
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-08-03 23:49:50 +0530

    solved valid anagram( leetcode 242)

---
+++ Strings/005_valid_anagram.cpp
@@ -0,0 +1,17 @@
+class solution{
+    public:
+
+    bool isanagram(string s, string t){
+        if(s.length()!= t.length()) return false;
+
+        int freq[26] = {0};
+
+        for(char c:s) freq[c-'a']++;
+        for(char c:t) freq[c-'a']--;
+
+        for(int count : freq){
+            if(count != 0) return false;
+        }
+        return true;
+    }
+}
