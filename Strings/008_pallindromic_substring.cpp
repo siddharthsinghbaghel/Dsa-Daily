b7fc84246de1fd77844f1fe20ea110594335c91b
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-08-05 23:35:07 +0530

    solved palindromic substrings (leetcode 647)

---
+++ Strings/008_pallindromic_substring.cpp
@@ -0,0 +1,20 @@
+class solution{
+    public:
+
+    int countsubstring(string s){
+        int n = s.size();
+        int count = 0;
+
+        for(int center = 0; center < 2*n - 1; center++){
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while(left >= 0 && right < n && s[left] == s[right]){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+    }
+}
