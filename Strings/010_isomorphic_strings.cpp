c5e401918cf797300c3ca20840a2f14b6b186de5
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-08-10 23:54:14 +0530

    solved isomorphic strings (leetcode 205)

---
+++ Strings/010_isomorphic_strings.cpp
@@ -0,0 +1,30 @@
+class solution{
+    public:
+
+    bool isIsomorphic(string s, string t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char, char> mp1, mp2;
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if(mp1.count(c1)){
+                if(mp1[c1] != c2){
+                    return false;
+                }
+            } else {
+                mp1[c1] = c2;
+            }
+            if(mp2.count(c2)){
+                if(mp2[c2] != c1){
+                    return false;
+                }
+            } else {
+                mp2[c2] = c1;
+            }
+        }
+        return true;
+    }
+}
