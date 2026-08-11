2f46a19c0a55b3444505fa34c816346d18b13da9
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-08-11 22:29:14 +0530

    update

--- Arrays/017_rotate_image.cpp
+++ Arrays/017_rotate_image.cpp
@@ -1,3 +1,6 @@
+class solution{
+    public:
+
 void rotate(vector<vector<int>> &matrix){
     int n = matrix.size();
     int m = matrix[0].size();
@@ -12,4 +15,6 @@ void rotate(vector<vector<int>> &matrix){
     for(int i =0; i<n; i++){
         reverse(matrix[i].begin(), matrix[i].end());
     }
+}
+
 }
