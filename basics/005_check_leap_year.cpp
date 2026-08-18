a8a07efa7015debb4fa10b684b49e877a01a82fa
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2026-08-18 14:24:42 +0530

    solved leap year

---
+++ basics/005_check_leap_year.cpp
@@ -0,0 +1,13 @@
+int main(){
+    int year;
+    cout<<"enter a year";
+    cin>>year;
+
+    if((year%4 == 0 && year%100 != 0) || (year%400 == 0)){
+        cout<<year<<" is a leap year";
+    }
+    else{
+        cout<<year<<" is not a leap year";
+    }
+    return 0;
+}
